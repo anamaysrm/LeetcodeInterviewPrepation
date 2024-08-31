72913ee6b2873edb8480b02c9205e28d6bcde433
Anamay Kumar Ojha <ojhaanamay@gmail.com>
2024-08-31 12:16:35 +0530

    Update lc27.kt

--- array/lc27.kt
+++ array/lc27.kt
@@ -32,5 +32,23 @@ Space Complexity -> O(1)
 
 Note :- This problem can be solved in place by just moving the elements which are equal to `val` at the end and bringing all the remaining elements to front by swapping technique.
 
+// 
 Approach 2 ->
+
+fun removeElement(nums: IntArray, `val`: Int): Int {
+      var index = 0
+
+      for(i in 0 until nums.size) {
+        if(nums[i]!=`val`) {
+            nums[index] = nums[i]
+            index++
+        }
+      }
+  return index
+    }
   
+Time Complexity -> O(nums.size)
+Space Complexity -> O(1)
+
+Note -> Itrate the whole array once with 2 indexs, one i to iterate the whole array and second "index" to place the non `val` elements in starting.
+
