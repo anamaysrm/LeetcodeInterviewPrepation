b28e7f2a7d26441e4ba12a9451ad29d0ee23d0a2
Anamay Kumar Ojha <ojhaanamay@gmail.com>
2024-08-31 14:22:58 +0530

    Create lc11.kt

---
+++ array/lc11.kt
@@ -0,0 +1,5 @@
+Leetcode Problem 11
+
+Link -> https://leetcode.com/problems/container-with-most-water/description/
+
+Was able to come up with the O(N^2) solution but not able to find the optimal approach.
